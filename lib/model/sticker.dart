81f948c650c774c853c054bae147413e2e478875
mustahsanK <mustahsan60@outlook.com>
2023-01-27 19:02:33 +0530

    Added sticker model class

---
+++ lib/model/sticker.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class Sticker {
+  final String name;
+  final String type;
+  final int price;
+  final Image img;
+
+  const Sticker(this.name, this.type, this.price, this.img);
+}
