bcd99675bb2f6f3ca2a95b95e2d6ccaa3b906f5b
mustahsanK <mustahsan60@outlook.com>
2023-02-01 23:52:11 +0530

    added files

---
+++ lib/views/widgets/sticker_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StickerList extends StatelessWidget {
+  const StickerList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/views/widgets/sticker_list_item.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StickerListItem extends StatelessWidget {
+  const StickerListItem({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
