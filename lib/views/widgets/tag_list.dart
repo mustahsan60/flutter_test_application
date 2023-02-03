258822eaf05dcbb0a7e984b89ca8530c733cdf2c
mustahsanK <mustahsan60@outlook.com>
2023-02-03 23:56:54 +0530

    Added file

---
+++ lib/views/widgets/tag_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TagList extends StatefulWidget {
+  const TagList({Key key}) : super(key: key);
+
+  @override
+  _TagListState createState() => _TagListState();
+}
+
+class _TagListState extends State<TagList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
