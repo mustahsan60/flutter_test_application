7474fd230824c5c8d6ff9d208caafafe8b378c5b
mustahsanK <mustahsan60@outlook.com>
2023-02-20 23:30:09 +0530

    implemented sticker list

--- lib/views/widgets/sticker_list_item.dart
+++ lib/views/widgets/sticker_list_item.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
 class StickerListItem extends StatelessWidget {
-  const StickerListItem({Key key}) : super(key: key);
+  const StickerListItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const AspectRatio(
+            aspectRatio: 18 / 11,
+            child: Image(
+              image: AssetImage('assets/images/img1.jpg'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('Flutter'),
+                SizedBox(height: 16.0),
+                Text('Price: 200'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
